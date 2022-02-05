a818da68e6f80e204b887fa0037b09492d989ece
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-05 11:20:30 +0300

    feat: add NewsViewController

---
+++ News/News/View Controllers/NewsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NewsViewController.swift
+//  News
+//
+//  Created by Станислав on 05.02.2022.
+//
+
+import UIKit
+
+class NewsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
