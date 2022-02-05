e0069f395b05b6a0891337a1e950f21706278b3a
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-05 17:16:50 +0300

    feat: show data in cells

--- News/News/View Controllers/NewsViewController.swift
+++ News/News/View Controllers/NewsViewController.swift
@@ -9,16 +9,40 @@ import UIKit
 
 class NewsViewController: UITableViewController {
 
+    // MARK: - Private Properties
+    private var news: News!
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetchData(from: Link(page: "1").api)
     }
 
     // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as! NewsTableViewCell
+        let article = news?.articles[indexPath.row]
+        cell.configure(with: article)
+        
+        return cell
+    }
+    
+    // MARK: - Private Methods
+    private func fetchData(from url: String) {
+        NetworkManager.shared.fetch(dataType: News.self, from: url) { result in
+            switch result {
+            case .success(let data):
+                self.news = data
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
+
