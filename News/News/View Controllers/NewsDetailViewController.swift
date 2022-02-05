038af95c3dffe66b96f559fcff446cb99bd9215e
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-05 19:20:45 +0300

    feat(NewsDetailVC): add NewsDetailViewController and IB Outlets

---
+++ News/News/View Controllers/NewsDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NewsDetailViewController.swift
+//  News
+//
+//  Created by Станислав on 04.02.2022.
+//
+
+import UIKit
+
+class NewsDetailViewController: UIViewController {
+
+    @IBOutlet var newsTitleLabel: UILabel!
+    @IBOutlet var descriptionNewsLabel: UILabel!
+    @IBOutlet var linkToNewsLabel: UILabel!
+    @IBOutlet var newsImage: NewsImageView!
+    @IBOutlet var viewsConter: UILabel!
+    
+    var article: Article!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newsTitleLabel.text = article.title
+        descriptionNewsLabel.text = article.description
+        linkToNewsLabel.text = article.url
+        
+        newsImage.fetchImage(from: article?.urlToImage ?? "")
+    }
+
+
+}
+
