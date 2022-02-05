af17ebbc4025204143cac707f92988e6600f30b9
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-05 10:09:23 +0300

    feat(model): add model for news

---
+++ News/News/Models/News.swift
@@ -0,0 +1,24 @@
+//
+//  News.swift
+//  News
+//
+//  Created by Станислав on 05.02.2022.
+//
+
+import Foundation
+
+struct News: Decodable {
+    let articles: Articles
+}
+
+struct Articles: Decodable {
+    let title: String
+    let description: String
+    let url: String
+    let urlToImage: String
+    let publishedAt: Date
+}
+
+enum Link: String {
+    case newsApi = "https://newsapi.org/v2/everything?domains=bloomberg.com&pageSize=100&apiKey=cc18ddf89acd415ea93001a81c07bc96"
+}
